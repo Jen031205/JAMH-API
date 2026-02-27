88c66d95cdfe5f8bc51894b8e58315c385eb9dde
Jennifer Ailin Medina Hernandez <jenniferailin031205@gmail.com>
2026-02-26 19:44:22 -0600

    fix: Correción de CRUD y uso de Swagger

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,7 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConnectConfig } from 'rxjs';
 import { CreateTaskDto } from 'src/modules/auth/dto/create-task.dto';
-import { updateTaskDto } from 'src/modules/auth/dto/update-task.dto';
+import { UpdateTaskDto } from 'src/modules/auth/dto/update-task.dto';
+import { Task } from 'src/modules/auth/entities/task.entity';
 
 @Injectable()
 export class TaskService {
@@ -12,21 +13,21 @@ export class TaskService {
 
     private task: any[] = [];
 
-    public async getTask(): Promise<any> {
-        const query = 'SELECT * FROM task';
+    public async getTask(): Promise<Task> {
+        const query = `SELECT * FROM task`;
         const [result]: any = await this.db.query(query);
 
         return result;
     }
 
-    public async getTaskById(id: number): Promise<any> {
-        const query = "SELECT * FROM task WHERE id='${ id }'";
+    public async getTaskById(id: number): Promise<Task> {
+        const query = `SELECT * FROM task WHERE id=${id}`;
         const [result] = await this.db.query(query)
-        return [0]
+        return result[0];
     }
 
-    public async insert(tasks: CreateTaskDto): Promise<any> {
-        const sql = "INSERT INTO tasks (name, description, priority, user_id)VALUES (${task.name}, ${task.description}, ${task.priority}, ${task.users_id} )"
+    public async insert(tasks: CreateTaskDto): Promise<Task> {
+        const sql = `INSERT INTO tasks (name, description, priority, user_id)VALUES ('${tasks.name}', '${tasks.description}', ${tasks.priority}, ${tasks.user_id})`
         const [result] = await this.db.query(sql);
         const insertId = result.insertId;
 
@@ -34,16 +35,29 @@ export class TaskService {
         return row;
     }
 
-    public async update(id: number, taskUpdate: updateTaskDto): Promise<any> {
+    public async update(id: number, taskUpdate: UpdateTaskDto): Promise<Task> {
         const task = await this.getTaskById(id);
+
         task.name = taskUpdate.name ? taskUpdate.name : task.name;
-        task.description = task.Update.description ?? task.description;
-        task.priority = task.Update.priority ?? task.priority;
+        task.description = taskUpdate.description ?? task.description;
+        task.priority = taskUpdate.priority ?? task.priority;
+
+        const query = `
+        UPDATE task 
+        SET name='${task.name}',
+         description='${task.description}',
+          priority=${task.priority}
+           WHERE id=${id}`
+
+        await this.db.query(query);
+        return await this.getTaskById(id);
     }
 
-    public delete(id: number): string {
-        const array = this.task.filter(t => t.id != id);
-        this.task = array;
-        return "Task Deleted";
+    public async delete(id: number): Promise<boolean> {
+        const query = `DELETE FROM task WHERE id=${id}`;
+        const [result] = await this.db.query(query);
+
+        return result.affectedRows > 0;
+
     }
 }
